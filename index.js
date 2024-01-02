2c9e464d319a7114c297816b8d344113a46e0454
Kayne <fegkay15@gmail.com>
2024-01-02 01:20:42 -0500

    Resolved custom status prevented update

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token, baseName, userID } = require('./config.json');
+const { token, baseName, userID, serverID } = require('./config.json');
+const cron = require('node-cron');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences] });
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-client.on('presenceUpdate', (oldPresence, newPresence) => {
-	if((newPresence.member.id == userID) && (newPresence.activities[0] != undefined) && (newPresence.activities[0].type == 0)){
-		client.user.setActivity(newPresence.activities[0].name)
-		newPresence.member.setNickname(baseName + " " + newPresence.activities[0].name.replace(/\s/g, ''));
-	}else if((newPresence.member.id == userID) && (newPresence.activities[0] == undefined)){
-		client.user.setPresence({ activity: null })
-		newPresence.member.setNickname(null);
-	}
+	cron.schedule('*/5 * * * * *', function() {
+		const guild = client.guilds.cache.get(serverID);
+		var john = guild.members.cache.get(userID);
+		var gameName;
+		for(i = 0; i < john.presence.activities.length; i++){
+			if(john.presence.activities[i].type == 0){
+				gameName = john.presence.activities[i].name;
+			}
+		}
+		if(gameName != undefined){
+			client.user.setActivity(gameName)
+			john.presence.member.setNickname(baseName + " " + gameName.replace(/\s/g, ''));
+		}else{
+			client.user.setPresence({ activity: null })
+			john.presence.member.setNickname(null);
+		}
+	});
 });
 
 client.login(token);
