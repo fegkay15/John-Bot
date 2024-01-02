e6db34f3c940807c9e47c9c35b2e34f1fa3130e6
Kayne <fegkay15@gmail.com>
2024-01-01 23:57:34 -0500

    Added matching of activity to that of the user

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
 	if((newPresence.member.id == userID) && (newPresence.activities[0] != undefined) && (newPresence.activities[0].type == 0)){
+		client.user.setActivity(newPresence.activities[0].name)
 		newPresence.member.setNickname(baseName + " " + newPresence.activities[0].name.replace(/\s/g, ''));
 	}else if((newPresence.member.id == userID) && (newPresence.activities[0] == undefined)){
+		client.user.setPresence({ activity: null })
 		newPresence.member.setNickname(null);
 	}
 });
